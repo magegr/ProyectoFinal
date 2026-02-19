0b71ce2288dca17d97a4036d29e10e290721f47e
Justo <jufeol@alumnatflorida.es>
2026-02-19 12:46:17 +0100

    BD actualizada

--- Backend/optica.sql
+++ Backend/optica.sql
@@ -1,9 +1,9 @@
 /*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-11.8.5-MariaDB, for debian-linux-gnu (aarch64)
+-- MariaDB dump 10.19-12.1.2-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: OpticaAdam
 -- ------------------------------------------------------
--- Server version	11.8.5-MariaDB-ubu2404
+-- Server version	12.1.2-MariaDB-ubu2404
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -72,7 +72,7 @@ CREATE TABLE `product` (
   `diameter` double DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,7 +83,7 @@ LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
 set autocommit=0;
 INSERT INTO `product` VALUES
-(1,'Air Optix','Sí',10,5,'lentilla',1,'Ciba Vision',NULL,NULL,NULL,NULL,'2','diaria',1.5,'lentilla1.jpg'),
+(1,'Air Optix','-',10,5,'lentilla',1,'Ciba Vision',NULL,NULL,NULL,NULL,'2','diaria',1.5,'lentilla1.jpg'),
 (2,'Total 30','-',10,4,'lentilla',1,'Alcon',NULL,NULL,NULL,NULL,'3','mensual',1.5,'lentilla2.jpg'),
 (3,'Lens 55','-',22,5,'lentilla',1,'Lens 55',NULL,NULL,NULL,NULL,'2','diaria',1.5,'lentilla3.jpg'),
 (4,'Air Toric','-',20,6,'lentilla',1,'UGO',NULL,NULL,NULL,NULL,'4','mensual',2,'lentilla4.jpg'),
@@ -98,7 +98,11 @@ INSERT INTO `product` VALUES
 (13,'Gafas de sol metal dorado degradadas','Gafas de sol de diseño elegante con montura metálica dorada y lentes degradadas. Modelo ligero y cómodo ideal para uso diario con protección solar y estilo moderno.',159,22,'sol',1,'Classic',NULL,'metal','unisex','dorado',NULL,NULL,NULL,'sol2.png'),
 (14,'Gafas de sol negras cuadradas oversize','Gafas de sol de diseño cuadrado oversize en color negro con montura gruesa de acetato y lentes degradadas. Modelo moderno y elegante ideal para protección solar diaria con estilo.',159,4,'sol',1,'Fashion','completa','acetato','mujer','negro',NULL,NULL,NULL,'sol3.png'),
 (15,'Gafas de sol ovaladas verdes retro','Gafas de sol de estilo retro con montura metálica fina y lentes verdes. Diseño ligero y elegante inspirado en el estilo vintage, ideal para uso diario con protección solar.',139,20,'sol',1,'Vintage','completa','metal','unisex','dorado',NULL,NULL,NULL,'sol4.png'),
-(16,'Gafas de sol ovaladas degradadas marrón','Gafas de sol de inspiración vintage con montura metálica dorada y lentes degradadas en tono marrón. Diseño sofisticado y ligero ideal para un look elegante diario con protección solar.',149,22,'sol',1,'Elegante','completa','metal','unisex','dorado',NULL,NULL,NULL,'sol5.png');
+(16,'Gafas de sol ovaladas degradadas marrón','Gafas de sol de inspiración vintage con montura metálica dorada y lentes degradadas en tono marrón. Diseño sofisticado y ligero ideal para un look elegante diario con protección solar.',149,22,'sol',1,'Elegante','completa','metal','unisex','dorado',NULL,NULL,NULL,'sol5.png'),
+(17,'Paño de microfibra','Ideal para la limpieza delicada de lentes y pantallas. Textura suave y no deja pelusas.',1.5,20,'accesorio',1,'-',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'accesorio1.png'),
+(18,'Funda rígida','Diseñada para proteger contra golpes y rayaduras. Interior aterciopelado suave.',11,15,'accesorio',1,'-',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'accesorio2.png'),
+(19,'Funda de tela','Ligera y flexible. Protege las lentes de arañazos y polvo.',5,20,'accesorio',1,'-',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'accesorio3.png'),
+(20,'Spray anti-huellas','Spray limpiador de gafas. Elimina el polvo, huellas y grasa sin dañar la montura. Apta para todos los cristales.',3,15,'accesorio',1,'Zeiss',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'accesorio4.png');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 commit;
@@ -177,4 +181,4 @@ commit;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2026-02-19 10:36:01
+-- Dump completed on 2026-02-19 11:45:35
