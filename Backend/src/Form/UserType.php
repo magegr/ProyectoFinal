35f202e3b21871472249f04d70534c5d048deab9
Justo <jufeol@alumnatflorida.es>
2026-02-20 09:06:46 +0100

    UserController creado

---
+++ Backend/src/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('name')
+            ->add('surname1')
+            ->add('surname2')
+            ->add('phone')
+            ->add('active')
+            ->add('createdAt', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('Relation', EntityType::class, [
+                'class' => Product::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
