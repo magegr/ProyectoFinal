1d743f38d714f1e3044557a429dd4b5b17568570
Justo <jufeol@alumnatflorida.es>
2026-02-20 11:39:22 +0100

    entidad citas

---
+++ Backend/src/Entity/Appointment.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AppointmentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AppointmentRepository::class)]
+class Appointment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $surname1 = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $surname2 = null;
+
+    #[ORM\Column]
+    private ?bool $firstTime = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $type = null;
+
+    #[ORM\Column]
+    private ?bool $acceptedTerms = null;
+
+    #[ORM\ManyToOne(inversedBy: 'appointments')]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname1(): ?string
+    {
+        return $this->surname1;
+    }
+
+    public function setSurname1(string $surname1): static
+    {
+        $this->surname1 = $surname1;
+
+        return $this;
+    }
+
+    public function getSurname2(): ?string
+    {
+        return $this->surname2;
+    }
+
+    public function setSurname2(?string $surname2): static
+    {
+        $this->surname2 = $surname2;
+
+        return $this;
+    }
+
+    public function isFirstTime(): ?bool
+    {
+        return $this->firstTime;
+    }
+
+    public function setFirstTime(bool $firstTime): static
+    {
+        $this->firstTime = $firstTime;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function isAcceptedTerms(): ?bool
+    {
+        return $this->acceptedTerms;
+    }
+
+    public function setAcceptedTerms(bool $acceptedTerms): static
+    {
+        $this->acceptedTerms = $acceptedTerms;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
